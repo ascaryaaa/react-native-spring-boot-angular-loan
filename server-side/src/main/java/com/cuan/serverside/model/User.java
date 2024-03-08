30a61c5b52fc113bcc032e536e2d78e34053564a
Kuntjo <vitokuntjoro@gmail.com>
2024-03-08 10:27:04 +0700

    add id

--- server-side/src/main/java/com/cuan/serverside/model/User.java
+++ server-side/src/main/java/com/cuan/serverside/model/User.java
@@ -1,5 +1,17 @@
 package com.cuan.serverside.model;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_pelanggan")
+    private Long idPelanggan;
 }
