18c67ba027d7d9f5ec5dfd3aa209290e8705891c
ascaryaaa <allaamascarya@gmail.com>
2024-03-08 18:44:45 +0700

    added login and home component and made a simple setup route

--- web-side/src/app/app.component.ts
+++ web-side/src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'web-side';
+  constructor(private router: Router) {}
+  
+  goHome() {
+    this.router.navigate(['/home']);
+  }
+  goLogin() {
+    this.router.navigate(['/login']);
+  }
 }
