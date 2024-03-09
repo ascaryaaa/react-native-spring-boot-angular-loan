7512d40f681f563be2efe77e82f27ca893434769
ascaryaaa <allaamascarya@gmail.com>
2024-03-09 07:34:19 +0700

    Connected api-endpoint from server-side (Spring-Boot) to mobile app (React-Native) and created SANBOX screen for sandboxing

--- mobile-side/App.js
+++ mobile-side/App.js
@@ -6,6 +6,7 @@ import Home from './src/screens/Home';
 import Login from './src/screens/Login';
 import DataDebitur from './src/screens/DataDebitur';
 import SimulasiPinjaman from './src/screens/SimulasiPinjaman';
+import SANDBOX from './src/screens/SANDBOX';
 
 const Stack = createStackNavigator();
 
@@ -13,7 +14,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator 
-        initialRouteName='Login'
+        initialRouteName='SANDBOX'
       >
         <Stack.Screen name="Home" 
           component={Home}
@@ -51,6 +52,17 @@ export default function App() {
         />
         <Stack.Screen name="DataDebitur" 
           component={DataDebitur}  
+          options={{ 
+            headerTintColor: 'white',
+            headerStyle: {
+              backgroundColor: '#39B54A',
+              shadowOpacity: 0,
+              elevation: 0,
+            }
+          }}  
+        />
+        <Stack.Screen name="SANDBOX" 
+          component={SANDBOX}  
           options={{ 
             headerTintColor: 'white',
             headerStyle: {
---
+++ mobile-side/src/screens/SANDBOX.jsx
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const SANDBOX = () => {
+  const [data, setData] = useState([]);
+  const url = 'http://192.168.234.54:8081/loan/v1/user/getUsers';
+
+  const fetchInfo = async () => {
+    try {
+      const response = await fetch(url);
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>User List</Text>
+      <View style={styles.dataContainer}>
+        {data.map((user, index) => (
+          <View key={index} style={styles.userContainer}>
+            <Text style={styles.userInfo}>{`ID: ${user.id_user}`}</Text>
+            <Text style={styles.userInfo}>{`Name: ${user.name_user}`}</Text>
+            <Text style={styles.userInfo}>{`NIK: ${user.nik_user}`}</Text>
+          </View>
+        ))}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  dataContainer: {
+    alignItems: 'center',
+  },
+  userContainer: {
+    backgroundColor: '#35D841',
+    padding: 10,
+    borderRadius: 10,
+    marginVertical: 10,
+    width: 250,
+  },
+  userInfo: {
+    fontSize: 16,
+    color: 'white',
+    marginBottom: 5,
+  },
+});
+
+export default SANDBOX;
