4caafcdc8457ea12801be4cb6bcfd2420da350f4
gerald7830 <geraldmhp@gmail.com>
2024-03-11 18:15:07 +0700

    frontend mobile progress

--- mobile-side/App.js
+++ mobile-side/App.js
@@ -1,137 +1,146 @@
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Home from './src/screens/Home';
-import Login from './src/screens/Login';
-import DataDebitur from './src/screens/DataDebitur';
-import SimulasiPinjaman from './src/screens/SimulasiPinjaman';
-import SANDBOX from './src/screens/SANDBOX';
-import DigitalLoan from './src/screens/DigitalLoan';
-import PengajuanPinjaman from './src/screens/PengajuanPinjaman';
-import DataPemohon from './src/screens/DataPemohon';
-import ProfileKeuangan from './src/screens/ProfileKeuangan';
-import SyaratKetentuan from './src/screens/SyaratKetentuan';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Home from "./src/screens/Home";
+import Login from "./src/screens/Login";
+import DataDebitur from "./src/screens/DataDebitur";
+import SimulasiPinjaman from "./src/screens/SimulasiPinjaman";
+import SANDBOX from "./src/screens/SANDBOX";
+import DigitalLoan from "./src/screens/DigitalLoan";
+import PengajuanPinjaman from "./src/screens/PengajuanPinjaman";
+import DataPemohon from "./src/screens/DataPemohon";
+import ProfileKeuangan from "./src/screens/ProfileKeuangan";
+import SyaratKetentuan from "./src/screens/SyaratKetentuan";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator 
-        initialRouteName='Login'
-      >
-        <Stack.Screen name="Home" 
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Home"
           component={Home}
-          options={{ 
-            headerTintColor: 'black',
+          options={{
+            headerTintColor: "black",
             headerShown: false,
             headerStyle: {
-              backgroundColor: '#FFFFFF',
-              shadowOpacity: 0,
-              elevation: 0,
-            }
-          }}  
-        />
-        <Stack.Screen name="Login" 
-          component={Login}  
-          options={{ 
-            headerTintColor: 'black',
-            headerStyle: {
-              backgroundColor: '#FFFFFF',
+              backgroundColor: "#FFFFFF",
               shadowOpacity: 0,
               elevation: 0,
             },
-          }}  
+          }}
         />
-        <Stack.Screen name="SimulasiPinjaman" 
-          component={SimulasiPinjaman}  
-          options={{ 
-            headerTintColor: 'black',
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            headerTintColor: "black",
+            headerShown: false,
             headerStyle: {
-              backgroundColor: '#FFFFFF',
+              backgroundColor: "#FFFFFF",
               shadowOpacity: 0,
               elevation: 0,
-            }
-          }}  
+            },
+          }}
         />
-        <Stack.Screen name="DataDebitur" 
-          component={DataDebitur}  
-          options={{ 
-            headerTintColor: 'black',
+        <Stack.Screen
+          name="SimulasiPinjaman"
+          component={SimulasiPinjaman}
+          options={{
+            headerTintColor: "black",
             headerStyle: {
-              backgroundColor: '#FFFFFF',
+              backgroundColor: "#FFFFFF",
               shadowOpacity: 0,
               elevation: 0,
-            }
-          }}  
+            },
+          }}
         />
-        <Stack.Screen name="SANDBOX" 
-          component={SANDBOX}  
-          options={{ 
-            headerTintColor: 'black',
+        <Stack.Screen
+          name="DataDebitur"
+          component={DataDebitur}
+          options={{
+            headerTintColor: "black",
             headerStyle: {
-              backgroundColor: '#FFFFFF',
+              backgroundColor: "#FFFFFF",
               shadowOpacity: 0,
               elevation: 0,
-            }
-          }}  
+            },
+          }}
         />
-        <Stack.Screen name="DigitalLoan" 
+        <Stack.Screen
+          name="SANDBOX"
+          component={SANDBOX}
+          options={{
+            headerTintColor: "black",
+            headerStyle: {
+              backgroundColor: "#FFFFFF",
+              shadowOpacity: 0,
+              elevation: 0,
+            },
+          }}
+        />
+        <Stack.Screen
+          name="DigitalLoan"
           component={DigitalLoan}
-          options={{ 
-            headerTintColor: 'black',
+          options={{
+            headerTintColor: "black",
             headerShown: false,
             headerStyle: {
-              backgroundColor: '#FFFFFF',
-              shadowOpacity: 0,
-              elevation: 0,
-            }
-          }}  
-        />
-        <Stack.Screen name="PengajuanPinjaman" 
-          component={PengajuanPinjaman}  
-          options={{ 
-            headerTintColor: 'black',
-            headerStyle: {
-              backgroundColor: '#FFFFFF',
+              backgroundColor: "#FFFFFF",
               shadowOpacity: 0,
               elevation: 0,
             },
-          }}  
+          }}
         />
-        <Stack.Screen name="DataPemohon" 
-          component={DataPemohon}  
-          options={{ 
-            headerTintColor: 'black',
+        <Stack.Screen
+          name="PengajuanPinjaman"
+          component={PengajuanPinjaman}
+          options={{
+            headerTintColor: "black",
             headerStyle: {
-              backgroundColor: '#FFFFFF',
+              backgroundColor: "#FFFFFF",
               shadowOpacity: 0,
               elevation: 0,
             },
-          }}  
+          }}
         />
-        <Stack.Screen name="ProfileKeuangan" 
-          component={ProfileKeuangan}  
-          options={{ 
-            headerTintColor: 'black',
+        <Stack.Screen
+          name="DataPemohon"
+          component={DataPemohon}
+          options={{
+            headerTintColor: "black",
             headerStyle: {
-              backgroundColor: '#FFFFFF',
+              backgroundColor: "#FFFFFF",
               shadowOpacity: 0,
               elevation: 0,
             },
-          }}  
+          }}
         />
-        <Stack.Screen name="SyaratKetentuan" 
-          component={SyaratKetentuan}  
-          options={{ 
-            headerTintColor: 'black',
+        <Stack.Screen
+          name="ProfileKeuangan"
+          component={ProfileKeuangan}
+          options={{
+            headerTintColor: "black",
             headerStyle: {
-              backgroundColor: '#FFFFFF',
+              backgroundColor: "#FFFFFF",
               shadowOpacity: 0,
               elevation: 0,
             },
-          }}  
+          }}
+        />
+        <Stack.Screen
+          name="SyaratKetentuan"
+          component={SyaratKetentuan}
+          options={{
+            headerTintColor: "black",
+            headerStyle: {
+              backgroundColor: "#FFFFFF",
+              shadowOpacity: 0,
+              elevation: 0,
+            },
+          }}
         />
       </Stack.Navigator>
     </NavigationContainer>
@@ -141,8 +150,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- mobile-side/src/screens/Login.jsx
+++ mobile-side/src/screens/Login.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
 
 const Login = ({navigation}) => {
+    
     return (
       <View style={styles.container}>
-        <Text>Login</Text>
+        <Image
+        source={require("../../../mobile-side/src/assets/bgawal.png")}
+        style={styles.background}
+      />
             <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('DigitalLoan')}>
                 <Text>Login</Text>
             </TouchableOpacity>
@@ -14,6 +18,11 @@ const Login = ({navigation}) => {
 export default Login;
 
 const styles = StyleSheet.create({
+    background: {
+        position: "absolute",
+        width: "100%",
+        height:"100%"
+      },
 container: {
     flex:1,
     justifyContent: 'center',
