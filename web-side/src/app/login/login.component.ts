d4fbea95fa298d809417004e19458aacf9868486
MRifqiWiliatama <mrifqiwiliatama160@gmail.com>
2024-03-13 13:12:10 +0700

    Login with NgForm

--- web-side/src/app/login/login.component.ts
+++ web-side/src/app/login/login.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-
+import { AuthService } from '../auth/auth.service';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  constructor(private service: AuthService) {}
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      alert("Please complete the form!");
+      return;
+    }
+    this.service.login(form.value)
+  }
 }
