c2fc532e78847a4ac7b87d4a2e0a325d4c0b362a
ascaryaaa <allaamascarya@gmail.com>
2024-03-14 08:14:44 +0700

    replace: replace the newest database

--- database/pengajuan.sql
+++ database/pengajuan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 13 Mar 2024 pada 10.03
+-- Waktu pembuatan: 14 Mar 2024 pada 02.12
 -- Versi server: 10.4.21-MariaDB
 -- Versi PHP: 8.0.10
 
@@ -20,6 +20,41 @@ SET time_zone = "+00:00";
 --
 -- Database: `pengajuan`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `account`
+--
+
+CREATE TABLE `account` (
+  `account_id` bigint(20) NOT NULL,
+  `account_name` varchar(255) DEFAULT NULL,
+  `account_pin` varchar(255) DEFAULT NULL,
+  `mpin` varchar(255) DEFAULT NULL,
+  `id_user` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` bigint(20) NOT NULL,
+  `name_admin` varchar(255) DEFAULT NULL,
+  `password_admin` varchar(255) DEFAULT NULL,
+  `npp_admin` varchar(255) DEFAULT NULL,
+  `username_admin` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `name_admin`, `password_admin`, `npp_admin`, `username_admin`) VALUES
+(1, 'Wahyu Khumairoh', '123456', 'T0672XX', 'admin');
 
 -- --------------------------------------------------------
 
@@ -139,9 +174,113 @@ INSERT INTO `cabang` (`id_cabang`, `kode_cabang`, `kota_cabang`, `name_cabang`) 
 (98, ' 9215', 'SEMARANG', 'BNI KARANGAYU-SUDIRMAN BRANCH'),
 (99, ' 9216', 'TULUNGAGUNG', 'BNI TLAGUNG-SUDIRMAN BRANCH');
 
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `form_pengajuan`
+--
+
+CREATE TABLE `form_pengajuan` (
+  `id_form_pengajuan_pinjaman` bigint(20) NOT NULL,
+  `alamat_ktp_user_form_pengajuan_pinjaman` varchar(255) DEFAULT NULL,
+  `bunga_pinjaman_form_pengajuan_pinjaman` double DEFAULT NULL,
+  `jangka_waktu_form_pengajuan_pinjaman` bigint(20) DEFAULT NULL,
+  `jenis_kelamin_form_pengajuan_pinjaman` varchar(255) DEFAULT NULL,
+  `jumlah_pinjaman_form_pengajuan_pinjaman` bigint(20) DEFAULT NULL,
+  `kecamatan_form_pengajuan_pinjaman` varchar(255) DEFAULT NULL,
+  `kelurahan_user_form_pengajuan_pinjaman` varchar(255) DEFAULT NULL,
+  `kode_pos_form_pengajuan_pinjaman` varchar(255) DEFAULT NULL,
+  `npwp_form_pengajuan_pinjaman` varchar(255) DEFAULT NULL,
+  `penghasilan_bersih_per_bulan_form_pengajuan_pinjaman` bigint(20) DEFAULT NULL,
+  `tanggal_lahir_form_pengajuan_pinjaman` varchar(255) DEFAULT NULL,
+  `tempat_lahir_form_pengajuan_pinjaman` varchar(255) DEFAULT NULL,
+  `unit_bni_terderkat_form_pengajuan_pinjaman` varchar(255) DEFAULT NULL,
+  `id_jenis_pinjaman` bigint(20) DEFAULT NULL,
+  `id_user` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jenis_pinjaman`
+--
+
+CREATE TABLE `jenis_pinjaman` (
+  `id_jenis_pinjaman` bigint(20) NOT NULL,
+  `gambar_jenis_pinjaman` varchar(255) DEFAULT NULL,
+  `icon_jenis_pinjaman` varchar(255) DEFAULT NULL,
+  `name_jenis_pinjaman` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `jenis_pinjaman`
+--
+
+INSERT INTO `jenis_pinjaman` (`id_jenis_pinjaman`, `gambar_jenis_pinjaman`, `icon_jenis_pinjaman`, `name_jenis_pinjaman`) VALUES
+(1, 'https://i.imgur.com/Ywn2QgH.png', 'https://i.imgur.com/MEcakIH.png', 'BNI_Griya'),
+(2, 'https://i.imgur.com/NDTf5NC.png', 'https://i.imgur.com/GZWNMR7.png', 'BNI_Fleksi'),
+(3, 'https://i.imgur.com/azUnAsh.png', 'https://i.imgur.com/MrKLSHu.png', 'BNI_Fleksi_Pensiun');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `promo`
+--
+
+CREATE TABLE `promo` (
+  `id_promo` bigint(20) NOT NULL,
+  `gambar_promo` varchar(255) DEFAULT NULL,
+  `name_promo` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `promo`
+--
+
+INSERT INTO `promo` (`id_promo`, `gambar_promo`, `name_promo`) VALUES
+(1, 'blob:https://imgur.com/c70d655a-a7d9-47af-a1d0-a2d827d4e307', 'Promo Fleksi 1'),
+(2, 'https://i.imgur.com/yjK6mpi.png', 'Promo Griya 1');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` bigint(20) NOT NULL,
+  `name_user` varchar(255) NOT NULL,
+  `nik_user` varchar(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id_user`, `name_user`, `nik_user`) VALUES
+(1, 'Sarah Johnson', '4829610329478516'),
+(2, 'Alex Wong', '7395124682053794'),
+(3, 'Emily Rodriguez', '6158930247156832'),
+(4, 'David Smith', '9264507315823496'),
+(5, 'Samantha Lee', '3721948520637149'),
+(6, 'Calestial Warrior', '8765209878192567');
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`account_id`),
+  ADD UNIQUE KEY `UK_mfisqiulqmeyv1vpx65qqcbvq` (`id_user`);
+
+--
+-- Indeks untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
 
 --
 -- Indeks untuk tabel `cabang`
@@ -150,14 +289,93 @@ ALTER TABLE `cabang`
   ADD PRIMARY KEY (`id_cabang`);
 
 --
+-- Indeks untuk tabel `form_pengajuan`
+--
+ALTER TABLE `form_pengajuan`
+  ADD PRIMARY KEY (`id_form_pengajuan_pinjaman`),
+  ADD KEY `FKpkpoy9vudj1olbxjmt4g19qv2` (`id_jenis_pinjaman`),
+  ADD KEY `FKl24wnhglox9dta3tex4wggflu` (`id_user`);
+
+--
+-- Indeks untuk tabel `jenis_pinjaman`
+--
+ALTER TABLE `jenis_pinjaman`
+  ADD PRIMARY KEY (`id_jenis_pinjaman`);
+
+--
+-- Indeks untuk tabel `promo`
+--
+ALTER TABLE `promo`
+  ADD PRIMARY KEY (`id_promo`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
+
+--
+-- AUTO_INCREMENT untuk tabel `account`
+--
+ALTER TABLE `account`
+  MODIFY `account_id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT untuk tabel `cabang`
 --
 ALTER TABLE `cabang`
   MODIFY `id_cabang` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100;
+
+--
+-- AUTO_INCREMENT untuk tabel `form_pengajuan`
+--
+ALTER TABLE `form_pengajuan`
+  MODIFY `id_form_pengajuan_pinjaman` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `jenis_pinjaman`
+--
+ALTER TABLE `jenis_pinjaman`
+  MODIFY `id_jenis_pinjaman` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `promo`
+--
+ALTER TABLE `promo`
+  MODIFY `id_promo` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `account`
+--
+ALTER TABLE `account`
+  ADD CONSTRAINT `FKr738a8wol7on2r69xf3iuvggx` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+
+--
+-- Ketidakleluasaan untuk tabel `form_pengajuan`
+--
+ALTER TABLE `form_pengajuan`
+  ADD CONSTRAINT `FKl24wnhglox9dta3tex4wggflu` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`),
+  ADD CONSTRAINT `FKpkpoy9vudj1olbxjmt4g19qv2` FOREIGN KEY (`id_jenis_pinjaman`) REFERENCES `jenis_pinjaman` (`id_jenis_pinjaman`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
