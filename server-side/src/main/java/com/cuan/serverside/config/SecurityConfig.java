5c249de12b99579b7516d53c2ea61847ae36f56a
ascaryaaa <allaamascarya@gmail.com>
2024-03-14 13:21:58 +0700

    refactor: refactor permitable path

--- server-side/src/main/java/com/cuan/serverside/config/SecurityConfig.java
+++ server-side/src/main/java/com/cuan/serverside/config/SecurityConfig.java
@@ -44,32 +44,14 @@ public class SecurityConfig  {
                 .requestMatchers("/loan/v1/promo/get-promos").permitAll() //Tambah path yang mau di permit
                 .requestMatchers("/loan/v1/cabang/get-cabangs").permitAll() //Tambah path yang mau di permit
                 .requestMatchers("loan/v1/jenis-pinjaman/get-jenis-pinjamans").permitAll()
-<<<<<<< HEAD
-<<<<<<< HEAD
+                .requestMatchers("loan/v1/account/get-accounts").permitAll()
                 .requestMatchers("loan/v1/account/post-account").permitAll()
-                .requestMatchers("loan/v1/account/post-account").permitAll()
-=======
-=======
-                .requestMatchers("loan/v1/account/post-account").permitAll()
-                .requestMatchers("loan/v1/account/post-account").permitAll()
->>>>>>> 90e6208f9d26b6fc5935f35d49cf90c2e1bd2ac9
                 .requestMatchers("loan/v1/admin/post-admin").permitAll()
                 .requestMatchers("loan/v1/admin/get-admins").permitAll()
                 .requestMatchers("loan/v1/user/post-user").permitAll()
                 .requestMatchers("loan/v1/user/get-users").permitAll()
                 .requestMatchers("loan/v1/pinjaman/get-pinjamans").permitAll()
                 .requestMatchers("loan/v1/pinjaman/post-pinjaman").permitAll()
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-                .requestMatchers("loan/v1/admin/get-admins").permitAll()
-                .requestMatchers("loan/v1/user/get-users").permitAll()
->>>>>>> rifqi
-=======
->>>>>>> 5a3cc191e16553132de9418fd8071b68227a2124
->>>>>>> e49da37e3e6a7604e2e5152007115b14ab41f8ee
-=======
->>>>>>> 90e6208f9d26b6fc5935f35d49cf90c2e1bd2ac9
                 .anyRequest().authenticated()
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
