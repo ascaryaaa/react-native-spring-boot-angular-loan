df9fc73a3b9257db11b80bc47fe6ba5fdb3edb7c
ascaryaaa <allaamascarya@gmail.com>
2024-03-18 15:32:04 +0700

    feat: implemented sidebar service to make the side bar can be hidden and shown in choosen component

--- web-side/src/app/app.component.ts
+++ web-side/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet, RouterModule } from '@angular/router';
+import { RouterOutlet, Router, RouterModule, NavigationEnd } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 import { LoginComponent } from './login/login.component';
 import { AuthService } from './auth/auth.service';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { SidebarService } from './sidebar/sidebar.service';
+import { Observable, filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -16,13 +17,29 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 })
 export class AppComponent implements OnInit {
   title = 'web-side';
+  sidebarVisible$!: Observable<boolean>;
   
   ngOnInit(): void {
     initFlowbite();
+    this.sidebarVisible$ = this.sidebarService.showSidebar$;
   }
 
   isLogin: boolean = false;
-  constructor(private service: AuthService) {
+
+  constructor(private service: AuthService, private router: Router, private sidebarService: SidebarService) {
     this.isLogin = !!this.service.getAuth();
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(event => {
+      // The event is now filtered but still needs a type guard
+      if (event instanceof NavigationEnd) {
+        // Now TypeScript knows `event` is definitely a NavigationEnd event
+        if (event.urlAfterRedirects === '/login') {
+          this.sidebarService.toggleSidebar(false);
+        } else {
+          this.sidebarService.toggleSidebar(true);
+        }
+      }
+    });
   }
 }
