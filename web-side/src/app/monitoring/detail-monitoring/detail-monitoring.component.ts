95bf1409987a71089448f2efa9a335ea41e7f5a0
MRifqiWiliatama <mrifqiwiliatama160@gmail.com>
2024-03-19 20:28:37 +0700

    update api detailing monitor

--- web-side/src/app/monitoring/detail-monitoring/detail-monitoring.component.ts
+++ web-side/src/app/monitoring/detail-monitoring/detail-monitoring.component.ts
@@ -4,7 +4,8 @@ import { DetailPinjamanResponse, Pinjaman } from '../monitoring';
 import { MonitoringService } from '../monitoring.service';
 import { Action } from 'rxjs/internal/scheduler/Action';
 import { ActivatedRoute } from '@angular/router';
-
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables)
 @Component({
   selector: 'app-detail-monitoring',
   templateUrl: './detail-monitoring.component.html',
@@ -18,17 +19,48 @@ export class DetailMonitoringComponent {
     this.refreshFormList();
   }
 
-  async refreshFormList() {
+
+  refreshFormList() {
     const id: number = this.router.snapshot.params['id'];
-    try{
-      this.monitoringService.getDetailMonitoringPinjaman(id).subscribe({
-        next: (data) => {
-          this.pinjamans = data;
-          console.log(this.pinjamans); // For debugging
-        },
-      })
-    } catch (error) {
-      console.error('Error fetching data:', error)
-    }
+    this.monitoringService.getDetailMonitoringPinjaman(id).subscribe({
+      next: (data) => {
+        this.pinjamans = data;
+        console.log(this.pinjamans); // For debugging
+        this.RenderChart(this.pinjamans.pinjamanToForm.jumlahPinjaman);
+      },
+      error: (error) => {
+        console.error('Error fetching data:', error);
+      }
+    });
+  }
+
+  RenderChart(pinjamansData: any) {
+    const myChart = new Chart("RenderChart",{
+      type: 'doughnut',
+      data: {
+        labels: [],
+        datasets:[{
+          label: '# of votes',
+          data: [100 - 25, 25], // Wrap pinjamansData in an array if it's not already an array
+          //rumus data [totalpinjaman-sisapinjaman, sisapinjaman]
+          backgroundColor: [
+            'rgb(246, 131, 16)',
+            'rgb(245, 245, 245)'
+          ],
+          borderColor: [
+            'rgb(246, 131, 16)',
+            'rgb(245, 245, 245)'
+          ],
+          borderWidth: 1
+        }] 
+      },
+      // options: {
+      //   scales: {
+      //     y: {
+      //       beginAtZero: true
+      //     }
+      //   }
+      // }
+    });
   }
 }
