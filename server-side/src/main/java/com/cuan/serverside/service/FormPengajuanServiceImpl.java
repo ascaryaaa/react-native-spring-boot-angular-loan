22d0386317572082075966d7604e0539a8568cff
Kuntjo <vitokuntjoro@gmail.com>
2024-03-20 13:14:09 +0700

    Added simulation function in Create Form

--- server-side/src/main/java/com/cuan/serverside/service/FormPengajuanServiceImpl.java
+++ server-side/src/main/java/com/cuan/serverside/service/FormPengajuanServiceImpl.java
@@ -1,7 +1,9 @@
 package com.cuan.serverside.service;
 
 import com.cuan.serverside.model.FormPengajuan;
+import com.cuan.serverside.model.JenisPinjaman;
 import com.cuan.serverside.repository.FormPengajuanRepository;
+import com.cuan.serverside.repository.JenisPinjamanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,9 +13,16 @@ public class FormPengajuanServiceImpl implements FormPengajuanService{
     // Dependency Injection, prevents coupling
     @Autowired
     private FormPengajuanRepository formPengajuanRepository;
-    public FormPengajuanServiceImpl(FormPengajuanRepository formPengajuanRepository) {
+    @Autowired
+    private JenisPinjamanRepository jenisPinjamanRepository;
+    public FormPengajuanServiceImpl(FormPengajuanRepository formPengajuanRepository, JenisPinjamanRepository jenisPinjamanRepository) {
         this.formPengajuanRepository = formPengajuanRepository;
+        this.jenisPinjamanRepository = jenisPinjamanRepository;
     }
+
+//    public FormPengajuanServiceImpl(JenisPinjamanRepository jenisPinjamanRepository) {
+//        this.jenisPinjamanRepository = jenisPinjamanRepository;
+//    }
 
     @Override
     public Iterable<FormPengajuan> getAllForm() {
@@ -27,6 +36,19 @@ public class FormPengajuanServiceImpl implements FormPengajuanService{
 
     @Override
     public FormPengajuan saveForm(FormPengajuan formPengajuan) {
+        formPengajuan.setStatusPengajuan("Diproses");
+
+        // Check if formToJenis is not null before accessing its properties
+        Long idJenis = formPengajuan.getFormToJenis().getIdJenisPinjaman();
+        JenisPinjaman jenisPinjaman = jenisPinjamanRepository.findById(idJenis).orElse(null);
+
+        // Perform calculations or other operations using jenisPinjamanName
+        Double bunga = jenisPinjaman.getBungaPinjaman();
+        Long jangka = formPengajuan.getJangkaWaktu();
+        Long maks = formPengajuan.getPenghasilanBersihPerbulan()/2;
+        Double result = (maks) * (1 - Math.pow(1 + (bunga/12), -jangka)) / (bunga/12);
+
+        formPengajuan.setMaksAngsuran(result);
         return formPengajuanRepository.save(formPengajuan);
     }
 
