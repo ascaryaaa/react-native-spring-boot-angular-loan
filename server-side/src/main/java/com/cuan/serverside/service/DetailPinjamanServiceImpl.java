49b873dc829efc5a8e081a7eba6287c0709911f0
Kuntjo <vitokuntjoro@gmail.com>
2024-03-20 22:06:22 +0700

    Added function auto naming on DetailPinjaman API

--- server-side/src/main/java/com/cuan/serverside/service/DetailPinjamanServiceImpl.java
+++ server-side/src/main/java/com/cuan/serverside/service/DetailPinjamanServiceImpl.java
@@ -1,7 +1,11 @@
 package com.cuan.serverside.service;
 
 import com.cuan.serverside.model.DetailPinjaman;
+import com.cuan.serverside.model.FormPengajuan;
+import com.cuan.serverside.model.User;
 import com.cuan.serverside.repository.DetailPinjamanRepository;
+import com.cuan.serverside.repository.FormPengajuanRepository;
+import com.cuan.serverside.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +14,15 @@ public class DetailPinjamanServiceImpl implements DetailPinjamanService{
     // Dependency Injection to prevent coupling
     @Autowired
     private DetailPinjamanRepository detailPinjamanRepository;
-    public DetailPinjamanServiceImpl(DetailPinjamanRepository detailPinjamanRepository) {
+    @Autowired
+    private FormPengajuanRepository formPengajuanRepository;
+    @Autowired
+    private UserRepository userRepository;
+    public DetailPinjamanServiceImpl(DetailPinjamanRepository detailPinjamanRepository, FormPengajuanRepository formPengajuanRepository, UserRepository userRepository) {
 
         this.detailPinjamanRepository = detailPinjamanRepository;
+        this.formPengajuanRepository = formPengajuanRepository;
+        this.userRepository = userRepository;
     }
 
     // Function overriding from interface
@@ -28,6 +38,13 @@ public class DetailPinjamanServiceImpl implements DetailPinjamanService{
 
     @Override
     public DetailPinjaman saveDetailPinjaman(DetailPinjaman detailPinjaman) {
+        Long idForm = detailPinjaman.getPinjamanToForm().getIdFormPengajuanPinjaman();
+        FormPengajuan formPengajuan = formPengajuanRepository.findById(idForm).orElse(null);
+        Long idUser = formPengajuan.getFormToUser().getIdUser();
+        User user = userRepository.findById(idUser).orElse(null);
+
+        detailPinjaman.setNameUser(user.getNameUser());
+        detailPinjaman.setNikUser(user.getNikUser());
         return detailPinjamanRepository.save(detailPinjaman);
     }
 
